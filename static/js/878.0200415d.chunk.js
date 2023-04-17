4317cb0a17e7ae4d28f555d35e79bbfa93a68c80
VitaliyMishugli <93128583+VitaliyMishugli@users.noreply.github.com>
2023-04-17 12:01:00 +0000

    Deploying to gh-pages from @ VitaliyMishugli/Phonebook@30ac63770db27f207447c755291d01910b1cbe92 🚀

---
+++ static/js/878.0200415d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[878],{1878:function(e,t,c){c.r(t);var o=c(184);t.default=function(){return(0,o.jsx)("h1",{children:'Page!!! Welcome to "Contact book" service!'})}}}]);
+//# sourceMappingURL=878.0200415d.chunk.js.map
