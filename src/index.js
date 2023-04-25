31d7a0ecb5fbc25c5e7984508ce97ff5d88f1305
Vitaliy Mishugli <vmishughli@gmail.com>
2023-04-25 14:06:55 +0100

    changed basename

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* <Paper> */}
           <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-              {/* <BrowserRouter basename="/Phonebook/"> */}
-              <BrowserRouter basename="/">
+              <BrowserRouter basename="/Phonebook/">
+              {/* <BrowserRouter basename="/"> */}
                 <App />
               </BrowserRouter>
             </PersistGate>
